cfa384724d856020e048dac885c6a1f4eb95b197
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-08-11 16:18:48 +0200

    project update knockoff

--- Frontend/src/app/components/learnbonus/learnbonus.component.ts
+++ Frontend/src/app/components/learnbonus/learnbonus.component.ts
@@ -4,52 +4,34 @@ import { GptService } from 'src/app/services/gpt.service';
 @Component({
   selector: 'app-learnbonus',
   templateUrl: './learnbonus.component.html',
-  styleUrls: ['./learnbonus.component.scss']
+  styleUrls: ['./learnbonus.component.scss'],
 })
 export class LearnbonusComponent implements OnInit {
+  constructor(private gptService: GptService) {}
 
-  constructor(private gptService: GptService) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   word: string = '';
   explanation: string = '';
   running = false;
-  
+
   async getMeaningOfWord() {
     try {
-      const response = await this.gptService.getMeaningOfWord(this.word).toPromise();
+      const response = await this.gptService
+        .getMeaningOfWord(this.word)
+        .toPromise();
       this.explanation = response.explanation;
     } catch (error) {
       console.error('Error fetching the meaning of the word:', error);
-      // Handle error
     }
   }
 
   onSubmit() {
-    this.getMeaningOfWord()
+    this.getMeaningOfWord();
   }
 
-  closeSearch(){
-    // this.running = true
-    this.explanation = ''
-    this.word = ''
+  closeSearch() {
+    this.explanation = '';
+    this.word = '';
   }
-
-  // onSubmit() {
-  //   this.gptservice
-  //     .learn(this.userInput)
-  //     .subscribe(
-  //       (data) => {
-  //         this.search = data.getExplanationForWord;
-  //         console.log(this.userInput)
-  //       },
-  //       (error) => {
-  //         console.error('Error fetching recommendations:', error);
-  //       }
-  //     );
-  // }
-
 }
