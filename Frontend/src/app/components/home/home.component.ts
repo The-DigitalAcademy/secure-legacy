b3253197ebbcf7116cdd9540d024e1d72e12f6c3
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-08-25 10:20:15 +0200

    hosting

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -11,5 +11,52 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  activeSlideIndex = 0; // Index of the initially active slide
+
+  carouselSlides = [
+    {
+      image: 'https://img.freepik.com/free-vector/group-people-illustration-set_52683-33806.jpg?w=2000',
+      title: 'Learn More About',
+      subtitle: 'Investments',
+      description: 'Get Started',
+    },
+    {
+      image: 'https://www.vanessaraath.com/wp-content/uploads/2023/03/people.jpg',
+      title: 'Learn More About',
+      subtitle: 'Life Cover',
+      description: 'Get Started',
+    },
+    {
+      image: 'https://img.freepik.com/free-vector/group-people-illustration-set_52683-33806.jpg?w=2000',
+      title: 'Learn More About',
+      subtitle: 'Disability Cover',
+      description: 'Get Started',
+    },
+    {
+      image: 'https://www.vanessaraath.com/wp-content/uploads/2023/03/people.jpg',
+      title: 'Learn More About',
+      subtitle: 'Educational Trust',
+      description: 'Get Started',
+    },
+    {
+      image: 'https://img.freepik.com/free-vector/group-people-illustration-set_52683-33806.jpg?w=2000',
+      title: 'Learn More About',
+      subtitle: 'Retirement Cover',
+      description: 'Get Started',
+    },
+    // Add more slides as needed
+  ];
+
+  nextSlide() {
+    this.activeSlideIndex = (this.activeSlideIndex + 1) % this.carouselSlides.length;
+  }
+
+  previousSlide() {
+    if (this.activeSlideIndex === 0) {
+      this.activeSlideIndex = this.carouselSlides.length - 1;
+    } else {
+      this.activeSlideIndex = (this.activeSlideIndex - 1) % this.carouselSlides.length;
+    }
+  }
 
 }
