3f26619711082cce655e7891b0f2df5661e3b177
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-08-25 11:04:25 +0200

    Hosting Project

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -15,31 +15,31 @@ export class HomeComponent implements OnInit {
 
   carouselSlides = [
     {
-      image: 'https://img.freepik.com/free-vector/group-people-illustration-set_52683-33806.jpg?w=2000',
+      image: 'https://ouch-cdn2.icons8.com/93j5jo8NI5fKk_BYSZCdfAkWBwzSK9hU4uNJ_-JEkJ8/rs:fit:256:384/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvODUw/LzgxZTdiMDRmLWQy/Y2QtNDYwNS05NDI4/LTcwZGU4NTdlNTZl/OC5wbmc.png',
       title: 'Learn More About',
       subtitle: 'Investments',
       description: 'Get Started',
     },
     {
-      image: 'https://www.vanessaraath.com/wp-content/uploads/2023/03/people.jpg',
+      image: 'https://ouch-cdn2.icons8.com/046iHL5EuDh4zqzlVMxaB5jPItz0kkvRwqCkYPPJfwE/rs:fit:256:218/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNTAv/YWNlZmI4ZWUtMGQ4/Ny00Y2E1LTgwZWMt/ZjQ5YTI5MzI4MDBj/LnBuZw.png',
       title: 'Learn More About',
       subtitle: 'Life Cover',
       description: 'Get Started',
     },
     {
-      image: 'https://img.freepik.com/free-vector/group-people-illustration-set_52683-33806.jpg?w=2000',
+      image: 'https://ouch-cdn2.icons8.com/cGfkg5FDjXEE9BOkSMPV3kuGkOeYP6XKZ6lvVwA3Nm8/rs:fit:256:312/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNzI5/LzhjNWY4MGIyLTYx/NGEtNDFhMi04ZjMz/LTg5ZjEyMGM5M2I3/YS5wbmc.png',
       title: 'Learn More About',
       subtitle: 'Disability Cover',
       description: 'Get Started',
     },
     {
-      image: 'https://www.vanessaraath.com/wp-content/uploads/2023/03/people.jpg',
+      image: 'https://ouch-cdn2.icons8.com/CAfThCeK-9AAimmPJrndwkdhVKEn9AxFf1Tw6-MkBlg/rs:fit:256:221/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMjcz/LzFmOWEzODY1LWZl/ZjQtNDAwYy1hMWQz/LTA4MjJiZTkyNmM5/MS5wbmc.png',
       title: 'Learn More About',
       subtitle: 'Educational Trust',
       description: 'Get Started',
     },
     {
-      image: 'https://img.freepik.com/free-vector/group-people-illustration-set_52683-33806.jpg?w=2000',
+      image: 'https://ouch-cdn2.icons8.com/9craA36nWy-8BCq_QPqcC6-A6lvDlAkhfUvAHVB-xMg/rs:fit:256:395/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMjg2/LzEzNWM4MDUyLWU2/MmEtNDQwMy1iMzQx/LWM1MTk3YWFiZjVk/OC5wbmc.png',
       title: 'Learn More About',
       subtitle: 'Retirement Cover',
       description: 'Get Started',
